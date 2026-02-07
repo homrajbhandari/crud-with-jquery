11423308b482ef7226a99f9c6dbbfa37071a270b
Hom Raj Bhandari <homrajbhandari96@gmail.com>
2026-02-07 09:39:03 +0545

    check/uncheck items to mark them as completed

--- js/app.js
+++ js/app.js
@@ -11,3 +11,13 @@ function render() {
 $(document).ready(function () {
   render();
 });
+
+function editCompleted(itemId) {
+  items = $.map(items, function (item) {
+    if (item.id === itemId) {
+      return $.extend({}, item, { completed: !item.completed });
+    }
+    return item;
+  });
+  render();
+}
